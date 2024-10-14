05133dfd39b1f90f2cb6e1ec3c19d07b87866235
emconno <emconno22@gmail.com>
2024-10-13 21:26:01 -0500

    messy and everything is broken

---
+++ database.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql")
+
+const pool = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database:'it391',
+    connectionLimit: 10
+});
+
+pool.connect(err => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Connected to database');
+    }
+});
+
+pool.query('select * from products', (err, res) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(res);
+    } 
+});
+
+pool.query('select * from products where product_name = \'cherry syrup\'', (err, res) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(res[0].sale_price);
+    } 
+});
